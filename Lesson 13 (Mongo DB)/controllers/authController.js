406202fcc6dc135eee24554462350af6e1e4d6a5
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-15 11:39:25 +0100

    update

--- Lesson 13 (Mongo DB)/controllers/authController.js
+++ Lesson 13 (Mongo DB)/controllers/authController.js
@@ -23,7 +23,7 @@ const handleLogin = async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "300s" }
+      { expiresIn: "30s" }
     );
     const refreshToken = jwt.sign(
       { username: foundUser.username },
